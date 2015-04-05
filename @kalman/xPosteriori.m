2bfad34ef3d263a621bdb31db15b42d7596b6d6f
lorienberne <lorienberne@gmail.com>
2015-04-05 12:20:06 +0200

    Delete xPosteriori.m

--- @kalman/xPosteriori.m
+++
@@ -1,9 +0,0 @@
-function xPost = xPosteriori(this,quad)
-%CONSTRUCT THE MEASUREMENT VECTOR FROM DATA FROM THE GYRO AND ACCELEROMETER
-    ac = this.accel.getAccelAngle(quad)
-    gy = this.gyro.gyroMeasure(quad);
-    y = [ac;gy];
-
-%%%%%%%%%%%%
-    xPost = this.xPriori + this.K * (y - C * this.xPriori);
-end
